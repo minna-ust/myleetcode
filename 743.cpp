7049c1e0ff2c73390a3372eb5fb0c2a5469098df
mingliang.wang <mingliang.wang@shmastermatrix.com>
2020-01-12 15:26:07 +0800

    [update hp]
    Signed-off-by: mingliang.wang <mingliang.wang@shmastermatrix.com>

---
+++ 743.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int networkDelayTime(vector<vector<int>>& times, int N, int K) {
+        map<int, vector<int, int>> graph;
+        for (const auto &time : times)
+        {
+            
+    }
+};
