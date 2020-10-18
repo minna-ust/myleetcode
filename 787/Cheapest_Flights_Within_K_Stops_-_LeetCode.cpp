f052d11c42af9d05430700773004ee0a34eab32c
minna-ust <wml.hust@gmail.com>
2020-10-18 18:08:30 +0800

    ma
    Signed-off-by: minna-ust <wml.hust@gmail.com>

--- 787/Cheapest_Flights_Within_K_Stops_-_LeetCode.cpp
+++ 787/Cheapest_Flights_Within_K_Stops_-_LeetCode.cpp
@@ -3,6 +3,9 @@
 #include <vector>
 #include <map>
 #include <boost/algorithm/string.hpp>
+#include <boost/algorithm/string_regex.hpp>
+// #include <boost/algorithm/string/regex.hpp>
+#include <boost/regex.hpp>
 
 class Solution {
 public:
@@ -23,18 +26,42 @@ public:
             }
         }
 
+        for (auto list : adjList)
+        {
+            std::cout << list.first << " \t";
+            for (auto pair : adjList[list.first])
+            {
+                std::cout << pair.first << " " << pair.second << "  ";
+            }
+            std::cout << std::endl;
+        }
 
     }
 };
 
 int main(int argc, char** argv)
 {
-    std::cout << argc  << std::endl;
     std::string input(argv[1]);
+    input = input.substr(1, input.size() - 2);
     input = input.substr(1, input.size() - 2);
 
     std::vector<std::string> vStr;
-    boost::split(vStr, input, boost::is_any_of(","));
+    // boost::split(vStr, input, boost::is_any_of("],["));
+    boost::algorithm::split_regex(vStr, input, boost::regex("\\],\\["));
 
+    std::vector<std::vector<int>> flights;
+    for (auto str : vStr)
+    {
+        std::vector<std::string> vFlight;
+        std::vector<int> flight;
+        boost::algorithm::split(vFlight, str, boost::is_any_of(","));
+        for (auto num : vFlight)
+        {
+            flight.push_back(std::stoi(num));
+        }
+        flights.push_back(flight);
+    }
+
+    Solution().findCheapestPrice(3, flights, 0, 2, 1);
     return 0;
 }
