319c656aaeec533128833bbbba782dff3e8605e5
minna-ust <wml.hust@gmail.com>
2021-03-02 11:52:55 +0800

    ma
    Signed-off-by: minna-ust <wml.hust@gmail.com>

--- 53/53.cpp
+++ 53/53.cpp
@@ -12,14 +12,28 @@
 |   |    |    |    |   |    |   |   |   |   |
 |   |    |    |    |   |    |   |   |   |   |
 
-
+       //当j = 0, i = 2时， sum = sums[i - 1][j] + nums[j + i - 1];
 
 -----------------------
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         int size = nums.size();
+        if (size == 0)
+            return 0;
         std::vector<int> vec(size, 0);
-        std::vector<std::vector<int>> res(vec, size + 1);
+        std::vector<std::vector<int>> sums(size + 1, vec);
+        int maxSum = INT_MIN;
+        for (int i = 1; i <= size; ++i)
+        {
+            for (int j = 0; j <= size - i; ++j)
+            {
+                // int sum = std::accumulate(nums.begin() + j, nums.begin() + j + i, 0);
+                int sum = sums[i - 1][j] + nums[j + i - 1];
+                sums[i][j] = sum;
+                maxSum = std::max(maxSum, sum);
+            }
+        }
+        return maxSum;
     }
 };
