cf29285889cffb4fb6854400093b76d7b9f6c3e6
minna-ust <wml.hust@gmail.com>
2021-03-05 13:47:30 +0800

    ma
    Signed-off-by: minna-ust <wml.hust@gmail.com>

--- other/imuAdjustDistortion/imuAdjustDistortion.cpp
+++ other/imuAdjustDistortion/imuAdjustDistortion.cpp
@@ -1,3 +1,28 @@
 [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] ---> ;
 [11, 2, 3, 4, 5, 6, 7, 8, 9, 10] ---> ;
 [11, 12, 3, 4, 5, 6, 7, 8, 9, 10] ---> ;
+-----------------------；
+情形1;
+[1, 2, 3, 4, somewhere, 5, 6, 7, 8, 9, 10] ---> ;
+情形2;
+[11, 12, 13, 14, 15, 16, 17, 8, somewhere, 9, 10] ---> ;
+-----------------------
+void adjustDistortion()
+{
+    while (imuPointerCur != imuPointerLast)
+    {
+        if (timeScanCur + pointTime < imuTime[imuPointerCur])
+        {
+            break;
+        }
+        imuPointerCur = (imuPointerCur + 1) % imuQueLength; // imuQueLength = 10;  情形1. imuPointerCur == 0; 情形2. imuPointerCur == 9;
+    }
+
+    if (timeScanCur + pointTime > imuTime[imuPointerCur])
+    {
+        //情形2, 此时 imuPointerCur ＝＝ imuPointerLast；
+        imuRollCur = imuRoll[imuPointerCur];
+        imuPitchCur = imuPitch[imuPointerCur];
+        imuYawCur = imuYaw[imuPointerCur];
+    } else { //情形1
+        int imuPointerBack = (imuPointerCur + imuQueLength - 1) % imuQueLength; // imuQueLength = 10; 情形1, imuPointerCur = 0; 情形2, imuPointerCur = 5;
