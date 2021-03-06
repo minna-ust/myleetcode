8ec3ad827a8ea7ccced1a080001a57996f6f0238
minna-ust <wml.hust@gmail.com>
2021-03-06 20:51:23 +0800

    ma
    Signed-off-by: minna-ust <wml.hust@gmail.com>

--- other/imuAdjustDistortion/imuAdjustDistortion.cpp
+++ other/imuAdjustDistortion/imuAdjustDistortion.cpp
@@ -1,9 +1,9 @@
-[1, 2, 3, 4, 5, 6, 7, 8, 9, 10] ---> ;
+[1, 2, 3, 4, 5, 6, 7, 0, 0, 0] ---> ;
 [11, 2, 3, 4, 5, 6, 7, 8, 9, 10] ---> ;
 [11, 12, 3, 4, 5, 6, 7, 8, 9, 10] ---> ;
 -----------------------；
 情形1;
-[1, 2, 3, 4, somewhere, 5, 6, 7, 8, 9, 10] ---> ;
+[1, 2, 3, 4, somewhere, 5, 6, 7, 0, 0, 0] ---> ;
 情形2;
 [11, 12, 13, 14, 15, 16, 17, 8, somewhere, 9, 10] ---> ;
 -----------------------
@@ -25,4 +25,21 @@ void adjustDistortion()
         imuPitchCur = imuPitch[imuPointerCur];
         imuYawCur = imuYaw[imuPointerCur];
     } else { //情形1
-        int imuPointerBack = (imuPointerCur + imuQueLength - 1) % imuQueLength; // imuQueLength = 10; 情形1, imuPointerCur = 0; 情形2, imuPointerCur = 5;
+        int imuPointerBack = (imuPointerCur + imuQueLength - 1) % imuQueLength;
+        // imuQueLength = 10; 情形1, imuPointerCur = 0; 情形2, imuPointerCur = 5;
+        float ratioFront = (timeScanCur + pointTime - imuTime[imuPointerBack])
+            / (imuTime[imuPointerFront] - imuTime[imuPointerBack]);
+        float ratioBack = (imuTime[imuPointerFront] - timeScanCur - pointTime)
+            / (imuTime[imuPointerFront] - imuTime[imuPointerBack]);
+
+        imuRollCur = imuRoll[imuPointerFront] * ratioFront + imuRoll[imuPointerBack] * ratioBack;
+        imuPitchCur = imuPitch[imuPointerFront] * ratioFront + imuPitch[imuPointerBack] * ratioBack;
+        if (imuYaw[imuPointerFront] - imuYaw[imuPointerBack] > M_PI) {
+            imuYawCur = imuYaw[imuPointerFront] * ratioFront + (imuYaw[imuPointerBack] + 2 * M_PI) * ratioBack;
+        } else if (imuYaw[imuPointerFront] - imuYaw[imuPointerBack] < -M_PI) {
+            imuYawCur = imuYaw[imuPointerFront] * ratioFront + (imuYaw[imuPointerBack] - 2 * M_PI) * ratioBack;
+        } else {
+            imuYawCur = imuYaw[imuPointerFront] * ratioFront + imuYaw[imuPointerBack] * ratioBack;
+        }
+        //...
+    }
