f748acb8270712ed6360952344412dbf30f86ff3
minna-ust <wml.hust@gmail.com>
2021-03-15 21:54:10 +0800

    ma
    Signed-off-by: minna-ust <wml.hust@gmail.com>

---
+++ 1143/1143.cpp
@@ -0,0 +1,27 @@
+                    bcde,ace;
+                        |
+                        |
+                        |
+                     bcd,ac
+                     /      \
+                    /       \
+                   /         \
+                 bc,ac     bcd,a
+                   |         /
+                  |         /
+                  |        /
+                 b,a     bc,a
+                           /
+                          /
+                         /
+                        b,a
+
+
+
+
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+
+    }
+};
