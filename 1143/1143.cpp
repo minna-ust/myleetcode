9961854b806e2f460ab57c39098d9398a4dc00d4
minna-ust <wml.hust@gmail.com>
2021-03-27 11:42:07 +0800

    ma
    Signed-off-by: minna-ust <wml.hust@gmail.com>

--- 1143/1143.cpp
+++ 1143/1143.cpp
@@ -49,6 +49,23 @@
 class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
-
+        int rows = text1.size() + 1;
+        int cols = text2.size() + 1;
+        vector<vector<int>> dp(rows, vector<int>(cols, 0));
+        for (int i = 1; i <= rows - 1; ++i)
+        {
+            for (int j = 1; j <= cols - 1; ++j)
+            {
+                if (text1[i - 1] == text2[j - 1]) //注意下标, 不是 text[i] == text[j]
+                {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+                else
+                {
+                    dp[i][j] = std::max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[rows - 1][cols - 1];
     }
 };
