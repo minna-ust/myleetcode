07b5a180a70f84a7310c14a616ec7f338dc29ffc
minna-ust <wml.hust@gmail.com>
2021-12-03 22:18:05 +0800

    ma

--- 3/3.cpp
+++ 3/3.cpp
@@ -1,13 +1,44 @@
+//abcbd
+//abcb
+//abc
+
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
+        if (s.size() == 0)
+            return 0;
+        if (s.size() == 1)
+            return 1;
         int max_len = 0;
-        std::set<int> st;
+        std::set<char> st;
         st.insert(s[0]);
+        int index_start = 0;
         for (int i = 1; i < s.size(); ++i)
         {
             if (st.find(s[i]) != st.end())
             {
-
+                for (int j = index_start; j < i; ++j)
+                {
+                    if (s[j] != s[i])
+                    {
+                        st.erase(s[j]);
+                    }
+                    else
+                    {
+                        max_len = std::max(max_len, i - index_start);
+                        index_start = j + 1;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                st.insert(s[i]);
+            }
+            if (i == s.size() - 1)
+            {
+                max_len = std::max(max_len, i - index_start);
+        }
+        return max_len;
     }
 };
