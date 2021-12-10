b9644ae31ed5426aef0a5bed118ad65440eebe44
minna <wml.hust@gmail.com>
2021-12-10 13:55:54 +0000

    ma

--- 133/133.cpp
+++ 133/133.cpp
@@ -1,1 +1,17 @@
 //直接在github上创建
+std::unordered_map<Node*,Node*> node_map;
+Node* cloneGraph(Node* node)
+{
+    if (node == nullptr)
+    {
+        return node;
+    }
+    if (node_map.count(node))
+        return node_map[node];
+    Node* cloned_node = new Node(node.val);
+    for ()
+    {
+
+    }
+
+}
