ebea616c1856e98e6d5602f87bbfe795abf605f7
minna <wml.hust@gmail.com>
2021-12-18 06:02:42 +0000

    ma

---
+++ 498/498.cpp
@@ -0,0 +1,33 @@
+vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
+    bool up = true;
+    int i = 0;
+    int j = 0;
+    int m = mat.size();
+    int n = mat[0].size();
+    vector<int> res;
+    while (!(i = m && j=n))
+    {
+        res.push_back(mat[i][j]);
+        if (up)
+        {
+            i--;
+            j++;
+        }
+        else
+        {
+            i++;
+            j--;
+        }
+        if (i < 0 && j < n)
+        {
+            up = false;
+            i++;
+        }
+        if (j < 0 && i < m)
+        {
+            up = true;
+            j++;
+        }
+        if (i
+    }
+}
