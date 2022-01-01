7409219c649bd48f5941dc176c43667e58ecb2ca
minna <wml.hust@gmail.com>
2022-01-01 14:42:51 +0000

    ma

--- 398/398.cpp
+++ 398/398.cpp
@@ -1,10 +1,23 @@
 class Solution {
 public:
     Solution(vector<int>& nums) {
-
+        nums_ = nums;
     }
 
     int pick(int target) {
-
+        int count = 1;
+        int res = -1;
+        for (int i = 0; i < nums_.size(); ++i)
+        {
+            if (nums_[i] == target)
+            {
+                if (rand() % count == 0)
+                {
+                    res = i;
+                }
+                count++;
+            }
+        }
+        return res;
     }
 };
