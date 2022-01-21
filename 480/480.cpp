f84824ceae425cd07e962633c708e8587555ec67
minna <wml.hust@gmail.com>
2022-01-21 05:06:36 +0000

    ma

--- 480/480.cpp
+++ 480/480.cpp
@@ -14,4 +14,69 @@ public:
         }
         return res;
     }
+
+    class DualPriorityQueue
+    {
+    private:
+        priority_queue<int,vector<int>,less<int>> small_q;
+        priority_queue<int,vector<int>,greater<int>> large_q;
+        std::unordered_map<int,int> delayed;
+        int small_size = 0;
+        int large_size = 0;
+    public:
+        void insert(int num)
+        {
+            if (small_q.empty() || num < small_q.top())
+            {
+                small_q.push(num);
+                make_balance();
+            }
+            else
+            {
+                large_q.push(num);
+                make_balance();
+            }
+        }
+
+        void erase(int num)
+        {
+            if (num = small_q.top())
+            {
+                small_q.pop();
+            }
+            if (num = large_q.pop())
+            {
+                large_q.pop();
+            }
+            delayed[num]++;
+            if (num < small_q.top())
+            {
+                small_size--;
+            }
+            if (num = large_q.pop())
+            {
+                large_size++;
+            }
+            make_balance();
+        }
+
+        void make_balance()
+        {
+            if (small_size > large_size + 1)
+            {
+                large_q.push(small_q.top());
+                small_q.pop();
+            }
+            if (small_size < large_size)
+            {
+                small_q.push(large_q.top());
+                large_q.pop();
+            }
+            prune();
+        }
+
+        void prune()
+        {
+
+
 };
