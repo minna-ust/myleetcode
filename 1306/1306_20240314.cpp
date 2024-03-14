89280aa1a17f1149f9e23a271391c3dd03825082
minna-ust <wml.hust@gmail.com>
2024-03-14 08:25:02 +0800

    ma

---
+++ 1306/1306_20240314.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canReach(vector<int>& arr, int start) {
+
+    }
+
+    bool recur(vector<int>& arr, int i) {
+        if (arr[i] == 0) {
+                return true;
+            }
+
+    }
+};
